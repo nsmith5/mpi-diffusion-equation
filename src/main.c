d0a3592a75d6f29f6335defbff5b69b6bee1d34c
Nathan Smith <nathan.smith5@mail.mcgill.ca>
2016-10-14 20:03:58 -0700

    Benchmark form

--- src/main.c
+++ src/main.c
@@ -19,7 +19,7 @@ int main (int argc, char **argv)
 {
     struct timespec t_start, t_end;
     double wtime;
-    int N = 2048;
+    int N = 8192;
     double dx = 0.1;
     double dt = 0.1;
     double D = 1.0;
@@ -46,8 +46,8 @@ int main (int argc, char **argv)
     for (int i = 0; i < 10; i++)
     {
       step (s);
-      save_state (s, file_id);
-      printf("Now on step %d\n", i);
+      //save_state (s, file_id);
+      //printf("Now on step %d\n", i);
     }
     clock_gettime (CLOCK_REALTIME, &t_end);
     wtime = (double)(t_end.tv_sec + t_end.tv_nsec*1e-9) -
